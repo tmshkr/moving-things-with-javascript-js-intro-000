4677522b35d33c347b676cd732921e97063b0ef0
Tim Shaker <timshaker@gmail.com>
2018-08-23 22:36:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ function moveDodgerLeft() {
   }
 }
 
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+
+  if (right > 0) {
+    dodger.style.right = `${left - 1}px`
+  }
+}
+
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
+  } else if (e.which === 39) {
+    moveDodgerRight()
   }
 })
